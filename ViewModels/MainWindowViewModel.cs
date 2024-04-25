82357173d01be22d47560e67cfdd1dda7f9ca302
Dmitriy <dimka59ru@gmail.com>
2024-04-25 23:01:38 +0500

    added panel buttons

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,40 +5,40 @@ namespace App.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-     private object? _currentView;
-        public object? CurrentView
+    private object? _currentView;
+    public object? CurrentView
+    {
+        get =>_currentView;
+        set
         {
-            get =>_currentView;
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _currentView, value);
-                RaiseProperties();
-            }
+            this.RaiseAndSetIfChanged(ref _currentView, value);
+            RaiseProperties();
         }
+    }
 
-        public ICommand OpenVideoPanelCommand { get; }
-        public ICommand PersonCommand { get; }
-        public ICommand MainSettingsCommand { get; }
-        
-        public bool IsVideoPanelSelected => CurrentView is VideoPanelViewModel;
-        public bool IsMainSettingSelected => CurrentView is MainSettingsViewModel;
-        
-        public MainWindowViewModel()
-        {
-            OpenVideoPanelCommand = ReactiveCommand.Create(VideoPanel);
-            PersonCommand = ReactiveCommand.Create(Person);
-            MainSettingsCommand = ReactiveCommand.Create(MainSettings);
+    public ICommand OpenVideoPanelCommand { get; }
+    public ICommand PersonCommand { get; }
+    public ICommand MainSettingsCommand { get; }
+    
+    public bool IsVideoPanelSelected => CurrentView is VideoPanelViewModel;
+    public bool IsMainSettingSelected => CurrentView is MainSettingsViewModel;
+    
+    public MainWindowViewModel()
+    {
+        OpenVideoPanelCommand = ReactiveCommand.Create(VideoPanel);
+        PersonCommand = ReactiveCommand.Create(Person);
+        MainSettingsCommand = ReactiveCommand.Create(MainSettings);
 
-            CurrentView = new VideoPanelViewModel();
-        }
-        
-        private void VideoPanel() => CurrentView = new VideoPanelViewModel();
-        private void Person() => CurrentView = new PersonViewModel();
-        private void MainSettings() => CurrentView = new MainSettingsViewModel();
+        CurrentView = new VideoPanelViewModel();
+    }
+    
+    private void VideoPanel() => CurrentView = new VideoPanelViewModel();
+    private void Person() => CurrentView = new PersonViewModel();
+    private void MainSettings() => CurrentView = new MainSettingsViewModel();
 
-        private void RaiseProperties()
-        {
-            this.RaisePropertyChanged(nameof(IsVideoPanelSelected));
-            this.RaisePropertyChanged(nameof(IsMainSettingSelected));
-        }
+    private void RaiseProperties()
+    {
+        this.RaisePropertyChanged(nameof(IsVideoPanelSelected));
+        this.RaisePropertyChanged(nameof(IsMainSettingSelected));
+    }
 }
---
+++ Views/GridButton.axaml.cs
@@ -0,0 +1,80 @@
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace App.Views;
+
+public partial class GridButton : UserControl
+{
+    private int _columnCount;
+    public static readonly DirectProperty<GridButton, int> ColumnCountProperty = AvaloniaProperty.RegisterDirect<GridButton, int>(
+        "ColumnCount", o => o.ColumnCount, (o, v) => o.ColumnCount = v);
+    public int ColumnCount
+    {
+        get => _columnCount;
+        set => SetAndRaise(ColumnCountProperty, ref _columnCount, value);
+    }
+
+    private int _rowCount;
+    public static readonly DirectProperty<GridButton, int> RowCountProperty = AvaloniaProperty.RegisterDirect<GridButton, int>(
+        "RowCount", o => o.RowCount, (o, v) => o.RowCount = v);
+    public int RowCount
+    {
+        get => _rowCount;
+        set => SetAndRaise(RowCountProperty, ref _rowCount, value);
+    }
+
+    public double ButtonWidth => 40;
+    public double ButtonHeight => 40;
+    
+    public ObservableCollection<int> Items { get; } = [];
+
+    private double _borderWidth;
+    public static readonly DirectProperty<GridButton, double> BorderWidthProperty = AvaloniaProperty.RegisterDirect<GridButton, double>(
+        nameof(BorderWidth), o => o.BorderWidth, (o, v) => o.BorderWidth = v);
+    public double BorderWidth
+    {
+        get => _borderWidth;
+        set => SetAndRaise(BorderWidthProperty, ref _borderWidth, value);
+    }
+    
+    private double _borderHeight;
+    public static readonly DirectProperty<GridButton, double> BorderHeightProperty = AvaloniaProperty.RegisterDirect<GridButton, double>(
+        nameof(BorderHeight), o => o.BorderHeight, (o, v) => o.BorderHeight = v);
+    public double BorderHeight
+    {
+        get => _borderHeight;
+        set => SetAndRaise(BorderHeightProperty, ref _borderHeight, value);
+    }
+    
+    public GridButton()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+        if (change.Property == ColumnCountProperty)
+        {
+            BorderWidth = ButtonWidth/ ColumnCount; 
+            AddItems();
+        }
+        else if (change.Property == RowCountProperty)
+        {
+            BorderHeight = ButtonHeight/ RowCount;
+            AddItems();
+        }
+    }
+
+    private void AddItems()
+    {
+        if (ColumnCount == 0 || RowCount == 0)
+            return;
+        for (var i = 0; i < ColumnCount * RowCount; i++)
+        {
+            Items.Add(i);
+        }
+    }
+}
