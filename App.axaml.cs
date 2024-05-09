c4a6e69c43bf05d76330219cef2d0b27288fa4bd
Dmitriy <dimka59ru@gmail.com>
2024-05-10 01:37:36 +0500

    init ffmpeg in app cs

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Runtime.InteropServices;
+using App.Services;
 using App.ViewModels;
 using App.Views;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using FFmpeg.AutoGen;
 
 namespace App;
 
@@ -10,6 +14,10 @@ public partial class App : Application
 {
     public override void Initialize()
     {
+        FFmpegBinariesHelper.RegisterFFmpegBinaries();
+        SetupFfmpegLogging();
+        Console.WriteLine($"FFmpeg version info: {ffmpeg.av_version_info()}");
+        
         AvaloniaXamlLoader.Load(this);
     }
 
@@ -24,5 +32,27 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+    
+    private static unsafe void SetupFfmpegLogging()
+    {
+        ffmpeg.av_log_set_level(ffmpeg.AV_LOG_VERBOSE);
+
+        // do not convert to local function
+        av_log_set_callback_callback logCallback = (p0, level, format, vl) =>
+        {
+            if (level > ffmpeg.av_log_get_level()) return;
+
+            var lineSize = 1024;
+            var lineBuffer = stackalloc byte[lineSize];
+            var printPrefix = 1;
+            ffmpeg.av_log_format_line(p0, level, format, vl, lineBuffer, lineSize, &printPrefix);
+            var line = Marshal.PtrToStringAnsi((IntPtr)lineBuffer);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(line);
+            Console.ResetColor();
+        };
+
+        ffmpeg.av_log_set_callback(logCallback);
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.Runtime.InteropServices;
-using App.Services;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
-using FFmpeg.AutoGen;
 using ReactiveUI;
 
 namespace App.ViewModels;
@@ -38,10 +35,6 @@ public class MainWindowViewModel : ViewModelBase
         
         this.WhenAnyValue(x => x.SelectedListItem)
             .Subscribe(OnSelectedListItemChanged);
-        
-        FFmpegBinariesHelper.RegisterFFmpegBinaries();
-        SetupFfmpegLogging();
-        Console.WriteLine($"FFmpeg version info: {ffmpeg.av_version_info()}");
     }
 
     private void OnSelectedListItemChanged(ListItemTemplate? value)
@@ -58,28 +51,6 @@ public class MainWindowViewModel : ViewModelBase
             true when type == typeof(SettingsPageViewModel) => new SettingsPageViewModel(),
             _ => CurrentPage
         };
-    }
-    
-    private static unsafe void SetupFfmpegLogging()
-    {
-        ffmpeg.av_log_set_level(ffmpeg.AV_LOG_VERBOSE);
-
-        // do not convert to local function
-        av_log_set_callback_callback logCallback = (p0, level, format, vl) =>
-        {
-            if (level > ffmpeg.av_log_get_level()) return;
-
-            var lineSize = 1024;
-            var lineBuffer = stackalloc byte[lineSize];
-            var printPrefix = 1;
-            ffmpeg.av_log_format_line(p0, level, format, vl, lineBuffer, lineSize, &printPrefix);
-            var line = Marshal.PtrToStringAnsi((IntPtr)lineBuffer);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write(line);
-            Console.ResetColor();
-        };
-
-        ffmpeg.av_log_set_callback(logCallback);
     }
 }
 
